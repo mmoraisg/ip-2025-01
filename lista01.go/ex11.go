362c64f06280ca253421055c9de933dd51f87024
Murilo Morais <moraisgomes@discente.ufg.br>
2025-04-04 14:02:15 -0300

    Create ex11.go

---
+++ lista01.go/ex11.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+    
+    if n%3==0 && n%5==0 {
+        fmt.Println("O NUMERO E DIVISIVEL")
+    } else {
+        fmt.Println("O NUMERO NAO E DIVISIVEL")
+    }
+}
