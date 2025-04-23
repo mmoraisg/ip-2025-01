7d5c7f623f179b9ac66712840354f662233f63f1
Murilo Morais <moraisgomes@discente.ufg.br>
2025-04-23 19:31:34 -0300

    Create ex01.go

---
+++ lista04/ex01.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var vetor [10]int
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("DIGITE O %dº NUMERO INTEIRO:", i+1)
+		fmt.Scan(&vetor[i])
+	}
+	encontrado := false
+
+	for i, num := range vetor {
+		if num > 50 {
+			fmt.Printf("O NUMERO %d DA POSICAO %d É MAIOR QUE 50\n", num, i+1)
+			encontrado = true
+		}
+	}
+	if !encontrado {
+		fmt.Println("NAO HA NUMEROS MAIORES QUE 50 NO VETOR")
+	}
+}
