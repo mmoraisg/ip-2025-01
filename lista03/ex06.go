5850f88effd7da7d1d1c7c29d995170cbb52a686
mmoraisg <moraisgomes@discente.ufg.br>
2025-04-29 18:03:22 -0300

    Mensagem do commit

--- lista03/ex06.go
+++ lista03/ex06.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package main
 
 import "fmt"
@@ -31,3 +32,38 @@ func main() {
 		fmt.Printf("%d nao e um numero triangular.", numero)
 	}
 }
+=======
+package main
+
+import "fmt"
+
+func numTriangular(n int) bool {
+	if n <= 0 {
+		return false
+	}
+	for i := 1; i*(i+1)*(i+2) <= n; i++ {
+		if i*(i+1)*(i+2) == n {
+			return true
+		}
+	}
+	return false
+}
+func main() {
+	var numero int
+
+	for {
+		fmt.Print("DIGITE UM NUMERO INTEIRO POSITIVO: ")
+		fmt.Scan(&numero)
+
+		if numero > 0 {
+			break
+		}
+		fmt.Println("NUMERO INVALIDO. TENTE NOVAMENTE.")
+	}
+	if numTriangular(numero) {
+		fmt.Printf("%d e um numero triangular.", numero)
+	} else {
+		fmt.Printf("%d nao e um numero triangular.", numero)
+	}
+}
+>>>>>>> e0fddb2 (Mensagem do commit)
