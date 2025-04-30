797a47eee241597b49b67bf254b5202dc2248e30
mmoraisg <moraisgomes@discente.ufg.br>
2025-04-30 11:14:13 -0300

    commitado

--- lista03/ex10.go
+++ lista03/ex10.go
@@ -5,9 +5,10 @@ import "fmt"
 func fibonacci(n int) int {
 	a, b := 0, 1
 	for i := 0; i < n; i++ {
-		fmt.Print(a, "")
+		fmt.Print(a, " ")
 		a, b = b, a+b
 	}
+	return a
 }
 func main() {
 	var n int
